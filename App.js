0188305713875fb8e48252d2fde6cd81a0a28088
Hansel Solevilla <sshanzel@yahoo.com>
2020-03-10 13:57:03 +0400

    removed everything except the button

--- App.js
+++ App.js
@@ -7,94 +7,32 @@
  */
 
 import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-  Button,
-} from 'react-native';
-
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import {View, Button} from 'react-native';
 
 import FP from './services/FingerprintModule';
 
 const App = () => {
   const handleTest = () => {
-    FP.open(() => {
-      console.log('success')
-    }, () => {
-      console.log('error')
-    })
-  }
+    FP.open(
+      () => {
+        console.log('success');
+      },
+      () => {
+        console.log('error');
+      },
+    );
+  };
 
   return (
-    <>
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <Button onPress={handleTest}><Text>Test</Text></Button>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-        <Footer />
-      </SafeAreaView>
-    </>
+    <View
+      style={{
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}>
+      <Button onPress={handleTest} title="Test" />
+    </View>
   );
 };
-
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
 
 export default App;
